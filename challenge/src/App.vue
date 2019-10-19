3ad6e36ccf4b51b0711544b052398b99b1d738e4
Zouhair ABOULHASSANE <zouhairaboulhassane@gmail.com>
2019-10-19 15:34:59 +0200

    Creating Project Vue CLI

---
+++ challenge/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Home page</h1>
+  </div>
+</template>
+<script>
+import Home from './views/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
