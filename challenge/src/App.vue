de4ee3991f94095e8518a5698a8cd9460eae7553
Zouhair ABOULHASSANE <zouhairaboulhassane@gmail.com>
2019-10-19 16:11:32 +0200

    Show navbar test

--- challenge/src/App.vue
+++ challenge/src/App.vue
@@ -1,18 +1,78 @@
 <template>
-  <div id="app">
-    <h1>Home page</h1>
+  <div id="in">
+    <header>
+    </header>
+    <nav>
+      <ul>
+        <li v-for="menu in menus" :key="menu.id"><a href="#">{{ menu.label }}</a></li>
+      </ul>
+    </nav>
   </div>
 </template>
 <script>
-import Home from './views/Home.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    Home
+  props: {
+    menus: Array
+  },
+  mounted() {
+    axios.get('http://femme.nextmedia.ma/api/menus/get_menu/?menu_id=7')
+      .then(response => {
+        this.menus = response.data.menu.output
+    })
   }
 }
 </script>
 <style lang="scss">
+@font-face {
+  font-family: 'Droid Arabic Kufi';
+  font-style: normal;
+  font-weight: 400;
+  src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.eot);
+  src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.eot?#iefix) format('embedded-opentype'),
+       url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.woff2) format('woff2'),
+       url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.woff) format('woff'),
+       url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Regular.ttf) format('truetype');
+}
+@font-face {
+  font-family: 'Droid Arabic Kufi';
+  font-style: normal;
+  font-weight: 700;
+  src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.eot);
+  src: url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.eot?#iefix) format('embedded-opentype'),
+       url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.woff2) format('woff2'),
+       url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.woff) format('woff'),
+       url(//fonts.gstatic.com/ea/droidarabickufi/v6/DroidKufi-Bold.ttf) format('truetype');
+}
 
+#in {
+  width: 90%;
+  margin: 0 auto;
+  font-family: 'Droid Arabic Kufi' , serif;
+  header {
+    width: 100%;
+    height: 70px;
+    border-bottom: 1px solid;
+  }
+  nav {
+    height: 50px;
+    background-color: gray;
+      ul {
+        margin: 0;
+        list-style-type: none;
+        float: right;
+        li {
+          display: inline-block;
+          padding: 10px;
+        }
+      }
+  }
+}
+@media only screen and (max-width: 780px) {
+  #in {
+    width: 100%;
+  }
+}
 </style>
